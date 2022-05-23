427a48027f790c94e1bbbdbfbf9efc65f26dbb1d
Aldair Leon <Aldair.Leon@blueyonder.com>
2022-05-23 09:54:45 -0500

    Add .gitignore

--- scripts/env_config.py
+++ scripts/env_config.py
@@ -21,13 +21,13 @@ def env_folder_path() -> str:
 
     """
     verify_path = os.path.exists(os.path.abspath("../snowflake/resources"))
-    # verify_path = os.path.exists(os.path.abspath("../resources"))
+    #verify_path = os.path.exists(os.path.abspath("../resources"))
     if verify_path:
         logger.info('Verify env file...')
     else:
         logger.error('Error env file, please verify your resource file!')
     return os.path.abspath("../snowflake/resources")
-    # return os.path.abspath("../resources")
+    #return os.path.abspath("../resources")
 
 
 # Env json file
@@ -83,3 +83,23 @@ def data_folder() -> str:
         logger.error('Error data folder doesnt exist, please verify your path!')
     return os.path.abspath("../snowflake/data")
     # return os.path.abspath("../data")
+
+
+# Data folder
+def entity_file() -> str:
+    """
+
+                ............
+
+    """
+    folder_path = env_folder_path()
+    verify_path = os.path.exists(os.path.abspath(folder_path + '/entities.json'))
+    if verify_path:
+        with open(folder_path + '/entities.json') as f:
+            entity = json.load(f)
+        logger.info('Loading entity file ...')
+        return entity
+
+    else:
+        logger.error('Error Loading entity file!')
+
