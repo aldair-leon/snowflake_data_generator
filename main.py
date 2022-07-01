529cd624f65bba78ca8580f47d354f4ca7fab806
Aldair Leon <Aldair.Leon@blueyonder.com>
2022-07-01 15:15:07 -0500

    commit

--- main.py
+++ main.py
@@ -37,15 +37,16 @@ from scripts.file_generation import *
 # Number of parameters = 1
 # env = DEV_PSR (Set as default )
 # """
-entity = 'inventorytransactions'  # items, locations, itemlocations, inventoryonhand, inventorytransactions,
+entity = 'itemlocations'  # items, locations, itemlocations, inventoryonhand, inventorytransactions,
 # itemhierarchylevelmembers, measurements
 
 env = 'DEV_PSR_ACCOUNT'
 env_azure = 'DEV_PSR'
 folder = 'processing'
+
 number_of_records = 10
 number_of_files = 1
-number_of_error_records = 0
+number_of_error_records = 5
 
 data_generation_create_data_main(entity, number_of_records, number_of_files, number_of_error_records)
 # azure_blob_upload_files(blob_container=env_azure, entity=entity)
